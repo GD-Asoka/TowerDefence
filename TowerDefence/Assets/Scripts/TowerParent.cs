0584dc1bc269c0ca4cc96cf65c83221ffb54f2c6
GD-Asoka <51798666+GD-Asoka@users.noreply.github.com>
2024-02-09 18:52:34 -0800

    1

--- TowerDefence/Assets/Scripts/TowerParent.cs
+++ TowerDefence/Assets/Scripts/TowerParent.cs
@@ -5,6 +5,32 @@ using UnityEngine;
 public class TowerParent : MonoBehaviour
 {
     [SerializeField] protected GameObject turret, projectile;
+    [SerializeField] protected SpriteRenderer sr;
+    [SerializeField] protected Rigidbody2D rb;
+    [SerializeField] protected Collider2D col;
+    protected Transform target;
 
-    
+    private void Awake()
+    {
+        turret = GetComponentInChildren<Turret>().gameObject;
+        projectile = GetComponentInChildren<Projectile>().gameObject;
+        rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<Collider2D>();
+    }
+
+    private void Start()
+    {
+        projectile.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+            target = collision.transform;
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        //StartCoroutine(Shoot());
+    }
 }
